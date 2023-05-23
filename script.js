918fc5f4aaa728d3bb0aacb2d36f1a0ff9c37461
lucianogargiulo <prueba@gmail.com>
2023-05-23 09:39:49 -0300

    validacion formulario

--- script.js
+++ script.js
@@ -46,3 +46,44 @@ fetch(urlDog)
   });
 
 
+  // Validacion formulario
+
+  const nombre = document.getElementById("nombre");
+  const email = document.getElementById("email");
+  const mensaje = document.getElementById("mensaje");
+  const form = document.getElementById("form");
+  const parrafo = document.getElementById("warning");
+
+  form.addEventListener("submit", e => {
+    e.preventDefault()
+    let warning = ""
+    let entrar = false
+    parrafo.innerHTML = ""
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+
+    if (nombre.value.length < 6) {
+      warning += `El nombre no es valido (min 6 caracteres) <br>`
+      entrar = true
+    }
+
+    console.log(regexEmail.test(email.value))
+    if (!regexEmail.test(email.value)){
+      warning += `El email no es valido <br>`
+      entrar = true
+    }
+
+    if (mensaje.value.length < 10) {
+      warning += `El mensaje no es valido (min 10 caracteres) <br>`
+      entrar = true
+    }
+
+    if (entrar) {
+      parrafo.innerHTML = warning
+    } else {
+      parrafo.innerHTML = "¡Enviado!"
+    }
+  })
+
+
+
+
